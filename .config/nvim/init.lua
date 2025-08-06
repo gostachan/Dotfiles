5083a34786aafa40eb2fc72d3b302716c9c9fba4
Hara Misaki <m.kevavu05@gmail.com>
2025-08-06 15:46:52 +0900

    .zshrc の変更とvimで検索の時大文字小文字を区別しないように変更

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -9,4 +9,7 @@ vim.opt.expandtab = false
 vim.opt.number = true
 vim.opt.clipboard:append({"unnamedplus"})
 vim.g.neovide_input_use_logo = true
+vim.opt.ignorecase = true  -- 通常は大文字小文字を無視
+vim.opt.smartcase  = true  -- 大文字を含むときのみ区別
+
 
