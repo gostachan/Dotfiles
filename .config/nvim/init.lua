82739cfc846db084c726959bb7cd46fddbef2a4a
Hara Misaki <m.kevavu05@gmail.com>
2025-08-14 16:57:21 +0900

    Fix

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -7,7 +7,6 @@ vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
 vim.opt.expandtab = false
 vim.opt.number = true
-vim.opt.clipboard:append({"unnamedplus"})
 vim.g.neovide_input_use_logo = true
 vim.opt.ignorecase = true  -- 通常は大文字小文字を無視
 vim.opt.smartcase  = true  -- 大文字を含むときのみ区別
