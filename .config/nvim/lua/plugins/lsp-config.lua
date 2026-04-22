29b12824cfc257927b9972d5e060b0f5ca0ae6df
gostachan <m.kevavu.prog@gmail.com>
2026-04-23 08:50:55 +0900

    save

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -51,7 +51,7 @@ return {
       local on_attach = function(_, bufnr)
         local opts = { noremap = true, silent = true, buffer = bufnr }
 
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+        vim.keymap.set("n", "gK", vim.lsp.buf.hover, opts)
         vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
         vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
         vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
